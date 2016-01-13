ffc9e3b913b0a642e59166f0f74994618b4cb922
roboprog <roboprog@int.bishopfoley.edu>
2016-01-12 20:00:57 -0500

    intial robot code

---
+++ src/org/usfirst/frc/team910/robot/DriveTrain.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc.team910.robot;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Talon;
+
+public class DriveTrain {
+
+	Talon lmTalon;
+	Talon rmTalon;
+
+	Encoder lEncoder;
+	Encoder rEncoder;
+
+	public DriveTrain() {
+		lmTalon = new Talon(0);
+		rmTalon = new Talon(1);
+		lEncoder = new Encoder(2, 3, false);
+		rEncoder = new Encoder(4, 5, false);
+	}
+
+	public void tankDrive(double YAxisLeft, double YAxisRight) {
+		lmTalon.set(YAxisLeft);
+		rmTalon.set(YAxisRight);
+
+	}
+}
