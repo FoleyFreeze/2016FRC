14c8c435a67779169ec8fc51bf5d7d9b024477c7
StevenSee <18scausley@bishopfoley.org>
2016-01-14 20:48:25 -0500

    drive straight added and stuffs

--- src/org/usfirst/frc/team910/robot/DriveTrain.java
+++ src/org/usfirst/frc/team910/robot/DriveTrain.java
@@ -1,15 +1,17 @@
 package org.usfirst.frc.team910.robot;
 
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Talon;
 
 public class DriveTrain {
-
+	
 	Talon lmTalon;
 	Talon rmTalon;
 
 	Encoder lEncoder;
 	Encoder rEncoder;
+
 	public DriveTrain() {
 		lmTalon = new Talon(0);
 		rmTalon = new Talon(1);
@@ -20,6 +22,26 @@ public class DriveTrain {
 	public void tankDrive(double YAxisLeft, double YAxisRight) {
 		lmTalon.set(YAxisLeft);
 		rmTalon.set(YAxisRight);
+	}
+
+	public void driveStraight(double lpower) {
+
+		double levalue;
+		double revalue;
+
+		levalue = lEncoder.get();
+		revalue = rEncoder.get();
+
+		double diff;
+
+		diff = levalue - revalue;
+
+		double adj = diff * 1;
+
+		double lnew = lpower - adj;
+		double rnew = lpower + adj;
+		tankDrive(lnew, rnew);
 
 	}
+
 }
