45ad26ff1efe249e3c29bcaefaf1657ae2389108
roboprog <roboprog@int.bishopfoley.edu>
2016-01-18 10:53:25 -0500

    added IO class, added Practice Bot Drive Train

--- src/org/usfirst/frc/team910/robot/IO.java
+++ src/org/usfirst/frc/team910/robot/IO.java
@@ -1,29 +1,26 @@
 package org.usfirst.frc.team910.robot;
 
-
-
 public class IO {
-	
+	// inputs
 	public static final int LEFT_DRIVE_MOTOR = 0;
 	public static final int RIGHT_DRIVE_MOTOR = 1;
-	
+
+	public static final int LEFT_DRIVE_A_ENCODER = 0;
+	public static final int RIGHT_DRIVE_A_ENCODER = 1;
+	public static final int LEFT_DRIVE_B_ENCODER = 2;
+	public static final int RIGHT_DRIVE_B_ENCODER = 3;
+
+	public static final int LEFT_GATHER_MOTOR = 4;
+	public static final int RIGHT_GATHER_MOTOR = 5;
+
+	public static final int SHOOTER_MOTOR = 6;
+
+	public static final int CLIMBER_ARM_MOTOR = 7;
+
+	// outputs
 	public static final int LEFT_JOYSTICK = 0;
 	public static final int RIGHT_JOYSTICK = 1;
-	
+
 	public static final int GAME_PAD = 2;
-	
-	public static final int LEFT_DRIVE_A_ENCODER= 0;
-	public static final int RIGHT_DRIVE_A_ENCODER = 1;
-	public static final int LEFT_DRIVE_B_ENCODER= 2;
-	public static final int RIGHT_DRIVE_B_ENCODER = 3;
-	
-	public static final int LEFT_GATHER_MOTOR = 4;
-	public static final int RIGHT_GATHER_MOTOR= 5;
-	
-	public static final int SHOOTER_MOTOR= 6;
-	
-	public static final int CLIMBER_ARM_MOTOR= 7;
-	
-	
-	
+
 }
