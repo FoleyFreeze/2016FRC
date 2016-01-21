6832876acb295b1a433eeba74941f077461792ae
Omega Drive <16Abaranski@bishopfoley.org>
2016-01-20 20:25:31 -0500

    Added Distance Sensor Functionality to Gatherer
    Added Distance Sensor Functionality to Gatherer

--- src/org/usfirst/frc/team910/robot/Gatherer.java
+++ src/org/usfirst/frc/team910/robot/Gatherer.java
@@ -1,10 +1,25 @@
 package org.usfirst.frc.team910.robot;
 
 import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Ultrasonic;
 
 public class Gatherer {
-	Talon gatherer;
+	boolean gatherdistance;
+	boolean release;
+	int gatherer;
 	Encoder garrettcoder;
-
+	Ultrasonic.Unit gatherersonic;{
+	if (gatherdistance) {
+		gatherer = 1;
+	}
+	else if (release){
+		gatherer = -1;
+	}
+	else {
+		gatherer = 0;
+		
+	} {
+	}
+	}
+	
 }
