69dd6f065eeb79151df11992a8654a67af92f028
Omega Drive <16Abaranski@bishopfoley.org>
2016-01-28 19:29:48 -0500

    Revert "Revert "Changed P from 1 to .25""
    This reverts commit 510238d366cb709461582e27ed15690e420c7a82.

--- src/org/usfirst/frc/team910/robot/DriveTrain.java
+++ src/org/usfirst/frc/team910/robot/DriveTrain.java
@@ -79,7 +79,7 @@ public class DriveTrain {
 
 			gooddiff = currentdiff - intdiff;
 
-			adj = gooddiff * 1;
+			adj = gooddiff * .25;
 
 			double lnew = lpower - adj;
 			double rnew = lpower + adj;
--- src/org/usfirst/frc/team910/robot/Gatherer.java
+++ src/org/usfirst/frc/team910/robot/Gatherer.java
@@ -1,25 +1,60 @@
 package org.usfirst.frc.team910.robot;
 
-import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Ultrasonic;
 
 public class Gatherer {
-	boolean gatherdistance;
+	boolean dungoofed;
+	boolean nextposition;
+	DigitalInput gatherdistance;
 	boolean release;
-	int gatherer;
-	Encoder garrettcoder;
-	Ultrasonic.Unit gatherersonic;{
-	if (gatherdistance) {
-		gatherer = 1;
+
+	CANTalon gatherer;
+	CANTalon gatherarm;
+	int state;
+
+	static final double ARM_DOWN = 446;
+	static final double ARM_UP = 893;
+	static final double ARM_CLOSE = 10;
+
+	public void gatherStateMachine() {
+		state = 1;
+		// determines initial position//
+		switch (state) {
+		case 1:
+			gatherer.set(0.7);
+			gatherarm.setPosition(ARM_DOWN);
+			// encoder count = setPosition()//
+			if (gatherarm.getPosition() > (ARM_DOWN - ARM_CLOSE) && gatherarm.getPosition() < (ARM_DOWN + ARM_CLOSE)
+					&& gatherdistance.get()) {
+				state = 2;
+			}
+			break;
+
+		default:
+			break;
+		case 2:
+			gatherarm.setPosition(ARM_UP);
+			if (gatherarm.getPosition() > (ARM_UP - ARM_CLOSE) && gatherarm.getPosition() < (ARM_UP + ARM_CLOSE)) {
+				state = 2;
+				}
+				break;
+			} else {
+				gatherer.set(.07);
+
+				break;
+			}
+		case 3:
+			if (gatherarm.equals(1339)) {
+				state = 3;
+				break;
+			} else {
+				gatherer.set(.07);
+				break;
+
+			}
+
+		}
 	}
-	else if (release){
-		gatherer = -1;
-	}
-	else {
-		gatherer = 0;
-		
-	} {
-	}
-	}
-	
 }
