a74dc3ea79673765d8e44e1a8a74de7262328c0a
Chase <19ccall@bishopfoley.org>
2016-02-08 19:43:08 -0500

    Changed Test to False
    Ready for real robot

--- src/org/usfirst/frc/team910/robot/Robot.java
+++ src/org/usfirst/frc/team910/robot/Robot.java
@@ -22,7 +22,7 @@ public class Robot extends IterativeRobot {
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
 	 */
-	static final boolean TEST = true;
+	static final boolean TEST = false;
 
 	DriveTrain drive;
 
