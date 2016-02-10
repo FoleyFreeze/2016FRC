2df936380ae805fcc2f6cf57b6e4df321cd62d5e
roboprog <roboprog@int.bishopfoley.edu>
2016-02-09 20:50:44 -0430

    added button functionality to BoulderController

--- src/org/usfirst/frc/team910/robot/BoulderController.java
+++ src/org/usfirst/frc/team910/robot/BoulderController.java
@@ -2,12 +2,42 @@ package org.usfirst.frc.team910.robot;
 
 public class BoulderController {
 
+	int button;
+	int layupBtn = button = 0;
+	int stowBtn = button = 1;
+	int farShotBtn = button = 2;
+	int gatherBtn = button = 3;
+	int primeBtn = button = 4;
+	int fireBtn = button = 5;
 	
+	public void runBC(boolean layupBtn, boolean stowBtn, boolean farShotBtn, boolean gatherBtn, boolean primeBtn, boolean fireBtn, double button){		
 	
-	public void runBC(boolean layup, boolean stow, boolean farShot, boolean gather, boolean prime, boolean fire){		
+		if (button == 0){ 
+			//set positions to layup on gatherer and shooter arms//
+		}
+		
+		else if (button == 1){
+			//set positions to stow on gatherer and shooter arms//
+		}
+		
+		else if (button == 2){
+			//set positions to farshot on gatherer and shooter arms//
+		}
 	
-	}
-	
+		else if (button == 3){
+			//set positions to gather on gatherer and shooter arms//
+		}
+		
+		else if (button == 4){
+			//set positions to prime on gatherer and shooter arms//
+		}
+		
+		else if (button == 5){
+			//set positions to fire on gatherer and shooter arms//
+		}
+		
+		
+	}	
 	public void layup(){
 		
 	}
